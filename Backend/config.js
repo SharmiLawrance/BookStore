c2ff25103c876278ee5a6da1f945ae05581add30
SharmiLawrance <151502597+SharmiLawrance@users.noreply.github.com>
2024-07-12 13:11:06 +0530

    Update config.js

--- Backend/config.js
+++ Backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 export const mongoDbUrl =
-  "mongodb+srv://sharmilawrance06:root123@bookstore.zproqad.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore";
+  "mongodb+srv://sharmilawrance06:<password>@bookstore.zproqad.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore";
